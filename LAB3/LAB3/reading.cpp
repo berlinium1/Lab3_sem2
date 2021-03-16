ea7fad29fbc36da9c80cf58e5dd604eef27c7c67
Anatoliy <tolxpams@gmail.com>
2021-03-17 00:37:22 +0200

    commenting

--- LAB3/LAB3/reading.cpp
+++ LAB3/LAB3/reading.cpp
@@ -7,7 +7,7 @@
 
 #include "reading.hpp"
 
-void toLittle (string *s) {
+void toLittle (string *s) { //функция которая меняет все буквы на строчные
     string sdop;
     sdop = *s;
     for (int i=0; i<sdop.size(); i++) {
@@ -18,29 +18,37 @@ void toLittle (string *s) {
     *s = sdop;
 }
 
-string readingCons () {
+string readingCons () { //функция чтения с консоли
     string sentence;
     getline(cin,sentence);
     toLittle(&sentence);
     return sentence;
 }
 
-void readingFile (string teka, hashTable *mas) {
+void readingFile (string teka, hashTable *mas, int *size) { //функция считывания словаря
     ///Users/khramchenko/Desktop/Reps/Lab3_sem2/files/dict_processed.txt
     hashTable res;
+    int count = 0;
     ifstream inFile;
     inFile.open(teka);
     string line;
     word word;
-    while (!inFile.eof()) {
-        word.key="";
-        getline(inFile,line,'\n');
-        int i;
-        for (i = 0; line[i] != ';'; i++) {
-            word.key = word.key + line[i];
+    if (inFile.is_open()) {
+        while (!inFile.eof()) {
+            count++;
+            word.key="";
+            getline(inFile,line,'\n'); //читаем строку
+            int i;
+            for (i = 0; line[i] != ';'; i++) {
+                word.key = word.key + line[i]; //то что до запятой - ключ
+            }
+            word.definition = line.substr(i+1,line.size()); //то что после - описание
+            toLittle(&word.key);
+            res.push_back(word); //добавляем в таблицу
         }
-        word.definition = line.substr(i+1,line.size());
-        toLittle(&word.key);
-        res.push_back(word);
+    } else {
+        cout << "file isnt open";
     }
+    *size = count;
+    *mas = res;
 }
